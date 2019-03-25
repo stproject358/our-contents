dcf9087ed44f5df784cf7453e00cbfe842c36043
Tanya Suchalkina <tatyana.suchalkina@gmail.com>
2019-03-25 11:33:19 +0200

    delete section

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+// Slider_our-contents
+
+	$('.slideshow-container').slick({
+		dots: true,
+  		infinite: true,
+  		speed: 500,
+  		fade: true,
+  		cssEase: 'linear'
+	});	
+});
